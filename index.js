5f2ba3c99a4be057c9926d4dea95b2e284af8e91
Nikola Muyot <nikola.muyot@gmail.com>
2024-03-27 15:33:00 -0400

    Implement license selection and badge integration

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Import the inquirer package
+// Import required packages
 const inquirer = require("inquirer");
+const fs = require("fs");
 
-// Array of questions for user input
+// Questions array for inquirer prompts
 const questions = [
   {
     type: "input",
@@ -13,17 +14,90 @@ const questions = [
     name: "description",
     message: "Provide a short description of your project:",
   },
-  // Add more questions as needed
+  {
+    type: "input",
+    name: "installation",
+    message: "Describe the installation process:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the project usage information?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "What are the contribution guidelines?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide test instructions:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
 ];
+
+// Function to generate README content based on user responses
+function generateREADME(answers) {
+  const licenseBadge =
+    answers.license !== "None"
+      ? `![License](https://img.shields.io/badge/license-${answers.license.replace(
+          " ",
+          "_"
+        )}-blue.svg)`
+      : "";
+
+  return `
+# ${answers.title}
+${licenseBadge}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contribution}
+
+## Tests
+${answers.tests}
+
+## License
+This project is licensed under the ${answers.license} license.
+`;
+}
 
 // Function to initialize the application
 function init() {
   inquirer.prompt(questions).then((answers) => {
-    console.log(answers);
-    // Here you will call a function to generate your README
-    // using the 'answers' object
+    const readmeContent = generateREADME(answers);
+
+    // Write the README content to a file
+    fs.writeFile("README.md", readmeContent, (err) => {
+      if (err) {
+        console.error("An error occurred:", err);
+        return;
+      }
+      console.log("README.md has been generated!");
+    });
   });
 }
 
-// Function call to initialize the application
+// Call the init function to start the application
 init();
